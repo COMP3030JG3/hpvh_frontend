879a226962cb3fc9b1523ffe53553bde9bfe059e
LiXu74 <1242947816@qq.com>
2020-03-21 09:42:06 +0800

    Login and singin

--- src/desktop/pages/users/Login/index.js
+++ src/desktop/pages/users/Login/index.js
@@ -1,7 +1,7 @@
 import React from "react";
-
+import LoginContainer from './LoginContainer'
 export default (props) => {
     return (
-        <div>Login</div>
+        <LoginContainer/>
     );
 };
