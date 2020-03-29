a65f9fea2b70564e0e26f39b8ccc9dde1fbd904b
Xingchen.Lu <wsteth@outlook.com>
2020-03-29 20:14:02 +0800

    add new home page

---
+++ src/desktop/pages/Home/data.source.js
@@ -0,0 +1,474 @@
+import React from 'react';
+export const Nav30DataSource = {
+  wrapper: { className: 'header3 home-page-wrapper jzih1dpqqrg-editor_css' },
+  page: { className: 'home-page k87mate2qet-editor_css' },
+  logo: {
+    className: 'header3-logo jzjgnya1gmn-editor_css',
+    children:
+      '/hvph.png',
+  },
+  Menu: {
+    className: 'header3-menu',
+    children: [
+      {
+        name: 'item1',
+        className: 'header3-item',
+        children: {
+          href: '#',
+          children: [
+            {
+              children: (
+                <span>
+                  <p>语言</p>
+                </span>
+              ),
+              name: 'text',
+              className: 'k87m1uuzaug-editor_css',
+            },
+          ],
+        },
+        subItem: [
+          {
+            className: 'item-sub',
+            children: {
+              className: 'item-sub-item jzj8295azrs-editor_css',
+              children: [
+                {
+                  name: 'content~k87m6p8ascf',
+                  className: '',
+                  children: (
+                    <span>
+                      <p>English</p>
+                    </span>
+                  ),
+                },
+              ],
+            },
+            name: 'sub~jzj8hceysgj',
+          },
+          {
+            className: 'item-sub',
+            children: {
+              className: 'item-sub-item jzj8295azrs-editor_css',
+              children: [
+                {
+                  name: 'content~k87m6p8ascf',
+                  className: '',
+                  children: (
+                    <span>
+                      <span>
+                        <p>中文</p>
+                      </span>
+                    </span>
+                  ),
+                },
+              ],
+            },
+            name: 'sub~k87m8ndibfn',
+          },
+        ],
+      },
+      {
+        name: 'item~k87ma5yvwvd',
+        className: 'header3-item',
+        children: {
+          href: '#',
+          children: [
+            {
+              children: (
+                <span>
+                  <span>
+                    <span>
+                      <p>用户</p>
+                    </span>
+                  </span>
+                </span>
+              ),
+              name: 'text',
+              className: 'k87me1dqtl-editor_css',
+            },
+          ],
+        },
+        subItem: [
+          {
+            className: 'item-sub',
+            children: {
+              className: 'item-sub-item jzj8295azrs-editor_css',
+              children: [
+                {
+                  name: 'content~k87m6p8ascf',
+                  className: '',
+                  children: (
+                    <span>
+                      <span>
+                        <p>注册</p>
+                      </span>
+                    </span>
+                  ),
+                },
+              ],
+            },
+            name: 'sub~jzj8hceysgj',
+          },
+          {
+            className: 'item-sub',
+            children: {
+              className: 'item-sub-item jzj8295azrs-editor_css',
+              children: [
+                {
+                  name: 'content~k87m6p8ascf',
+                  className: '',
+                  children: (
+                    <span>
+                      <span>
+                        <span>
+                          <p>登录</p>
+                        </span>
+                      </span>
+                    </span>
+                  ),
+                },
+              ],
+            },
+            name: 'sub~k87m8ndibfn',
+          },
+        ],
+      },
+    ],
+  },
+  mobileMenu: { className: 'header3-mobile-menu' },
+};
+export const Banner50DataSource = {
+  wrapper: { className: 'home-page-wrapper banner5' },
+  page: { className: 'home-page banner5-page' },
+  childWrapper: {
+    className: 'banner5-title-wrapper',
+    children: [
+      { name: 'title', children: '产品名', className: 'banner5-title' },
+      {
+        name: 'explain',
+        className: 'banner5-explain',
+        children: '产品标语介绍',
+      },
+      {
+        name: 'content',
+        className: 'banner5-content',
+        children: '产品的详细说明，如是什么东西之类的文字',
+      },
+      {
+        name: 'button',
+        className: 'banner5-button-wrapper',
+        children: {
+          href: '#',
+          className: 'banner5-button',
+          type: 'primary',
+          children: '开始使用',
+        },
+      },
+    ],
+  },
+  image: {
+    className: 'banner5-image',
+    children:
+      'https://gw.alipayobjects.com/mdn/rms_ae7ad9/afts/img/A*-wAhRYnWQscAAAAAAAAAAABkARQnAQ',
+  },
+};
+export const Feature30DataSource = {
+  wrapper: { className: 'home-page-wrapper content3-wrapper' },
+  page: { className: 'home-page content3' },
+  OverPack: { playScale: 0.3 },
+  titleWrapper: {
+    className: 'title-wrapper',
+    children: [
+      {
+        name: 'title',
+        children: '蚂蚁金融云提供专业的服务',
+        className: 'title-h1',
+      },
+      {
+        name: 'content',
+        className: 'title-content',
+        children: '基于阿里云强大的基础资源',
+      },
+    ],
+  },
+  block: {
+    className: 'content3-block-wrapper',
+    children: [
+      {
+        name: 'block0',
+        className: 'content3-block',
+        md: 8,
+        xs: 24,
+        children: {
+          icon: {
+            className: 'content3-icon',
+            children:
+              'https://zos.alipayobjects.com/rmsportal/ScHBSdwpTkAHZkJ.png',
+          },
+          textWrapper: { className: 'content3-text' },
+          title: { className: 'content3-title', children: '企业资源管理' },
+          content: {
+            className: 'content3-content',
+            children:
+              '云资源集中编排、弹性伸缩、持续发布和部署，高可用及容灾。',
+          },
+        },
+      },
+      {
+        name: 'block1',
+        className: 'content3-block',
+        md: 8,
+        xs: 24,
+        children: {
+          icon: {
+            className: 'content3-icon',
+            children:
+              'https://zos.alipayobjects.com/rmsportal/NKBELAOuuKbofDD.png',
+          },
+          textWrapper: { className: 'content3-text' },
+          title: { className: 'content3-title', children: '云安全' },
+          content: {
+            className: 'content3-content',
+            children:
+              '按金融企业安全要求打造的完整云上安全体系，全方位保障金融应用及数据安全。',
+          },
+        },
+      },
+      {
+        name: 'block2',
+        className: 'content3-block',
+        md: 8,
+        xs: 24,
+        children: {
+          icon: {
+            className: 'content3-icon',
+            children:
+              'https://zos.alipayobjects.com/rmsportal/xMSBjgxBhKfyMWX.png',
+          },
+          textWrapper: { className: 'content3-text' },
+          title: { className: 'content3-title', children: '云监控' },
+          content: {
+            className: 'content3-content',
+            children:
+              '分布式云环境集中监控，统一资源及应用状态视图，智能分析及故障定位。',
+          },
+        },
+      },
+      {
+        name: 'block3',
+        className: 'content3-block',
+        md: 8,
+        xs: 24,
+        children: {
+          icon: {
+            className: 'content3-icon',
+            children:
+              'https://zos.alipayobjects.com/rmsportal/MNdlBNhmDBLuzqp.png',
+          },
+          textWrapper: { className: 'content3-text' },
+          title: { className: 'content3-title', children: '移动' },
+          content: {
+            className: 'content3-content',
+            children:
+              '一站式移动金融APP开发及全面监控；丰富可用组件，动态发布和故障热修复。',
+          },
+        },
+      },
+      {
+        name: 'block4',
+        className: 'content3-block',
+        md: 8,
+        xs: 24,
+        children: {
+          icon: {
+            className: 'content3-icon',
+            children:
+              'https://zos.alipayobjects.com/rmsportal/UsUmoBRyLvkIQeO.png',
+          },
+          textWrapper: { className: 'content3-text' },
+          title: { className: 'content3-title', children: '分布式中间件' },
+          content: {
+            className: 'content3-content',
+            children:
+              '金融级联机交易处理中间件，大规模分布式计算机，数万笔/秒级并发能力，严格保证交易数据统一性。',
+          },
+        },
+      },
+      {
+        name: 'block5',
+        className: 'content3-block',
+        md: 8,
+        xs: 24,
+        children: {
+          icon: {
+            className: 'content3-icon',
+            children:
+              'https://zos.alipayobjects.com/rmsportal/ipwaQLBLflRfUrg.png',
+          },
+          textWrapper: { className: 'content3-text' },
+          title: { className: 'content3-title', children: '大数据' },
+          content: {
+            className: 'content3-content',
+            children:
+              '一站式、全周期大数据协同工作平台，PB级数据处理、毫秒级数据分析工具。',
+          },
+        },
+      },
+    ],
+  },
+};
+export const Feature50DataSource = {
+  wrapper: { className: 'home-page-wrapper content7-wrapper' },
+  page: { className: 'home-page content7' },
+  OverPack: {},
+  titleWrapper: {
+    className: 'title-wrapper',
+    children: [
+      {
+        name: 'title',
+        children: '蚂蚁金融云提供专业的服务',
+        className: 'title-h1',
+      },
+      { name: 'content', children: '基于阿里云计算强大的基础资源' },
+    ],
+  },
+  tabsWrapper: { className: 'content7-tabs-wrapper' },
+  block: {
+    children: [
+      {
+        name: 'block0',
+        tag: {
+          className: 'content7-tag',
+          text: { children: 'PHONE', className: 'content7-tag-name' },
+          icon: { children: 'mobile' },
+        },
+        content: {
+          className: 'content7-content',
+          text: {
+            className: 'content7-text',
+            md: 14,
+            xs: 24,
+            children: (
+              <span>
+                <h3>技术</h3>
+                <p>
+                  丰富的技术组件，简单组装即可快速搭建金融级应用，丰富的技术组件，简单组装即可快速搭建金融级应用。
+                </p>
+                <br />
+                <h3>融合</h3>
+                <p>
+                  解放业务及技术生产力，推动金融服务底层创新，推动金融服务底层创新。解放业务及技术生产力，推动金融服务底层创新。
+                </p>
+                <br />
+                <h3>
+                  开放
+                </h3>符合金融及要求的安全可靠、高可用、高性能的服务能力，符合金融及要求的安全可靠、高可用、高性能的服务能力。
+              </span>
+            ),
+          },
+          img: {
+            className: 'content7-img',
+            children:
+              'https://zos.alipayobjects.com/rmsportal/xBrUaDROgtFBRRL.png',
+            md: 10,
+            xs: 24,
+          },
+        },
+      },
+      {
+        name: 'block1',
+        tag: {
+          className: 'content7-tag',
+          icon: { children: 'tablet' },
+          text: { className: 'content7-tag-name', children: 'TABLET' },
+        },
+        content: {
+          className: 'content7-content',
+          text: {
+            className: 'content7-text',
+            md: 14,
+            xs: 24,
+            children: (
+              <span>
+                <h3>技术</h3>
+                <p>
+                  丰富的技术组件，简单组装即可快速搭建金融级应用，丰富的技术组件，简单组装即可快速搭建金融级应用。
+                </p>
+                <br />
+                <h3>融合</h3>
+                <p>
+                  解放业务及技术生产力，推动金融服务底层创新，推动金融服务底层创新。解放业务及技术生产力，推动金融服务底层创新。
+                </p>
+                <br />
+                <h3>
+                  开放
+                </h3>符合金融及要求的安全可靠、高可用、高性能的服务能力，符合金融及要求的安全可靠、高可用、高性能的服务能力。
+              </span>
+            ),
+          },
+          img: {
+            className: 'content7-img',
+            md: 10,
+            xs: 24,
+            children:
+              'https://zos.alipayobjects.com/rmsportal/xBrUaDROgtFBRRL.png',
+          },
+        },
+      },
+      {
+        name: 'block2',
+        tag: {
+          className: 'content7-tag',
+          text: { children: 'DESKTOP', className: 'content7-tag-name' },
+          icon: { children: 'laptop' },
+        },
+        content: {
+          className: 'content7-content',
+          text: {
+            className: 'content7-text',
+            md: 14,
+            xs: 24,
+            children: (
+              <span>
+                <h3>技术</h3>
+                <p>
+                  丰富的技术组件，简单组装即可快速搭建金融级应用，丰富的技术组件，简单组装即可快速搭建金融级应用。
+                </p>
+                <br />
+                <h3>融合</h3>
+                <p>
+                  解放业务及技术生产力，推动金融服务底层创新，推动金融服务底层创新。解放业务及技术生产力，推动金融服务底层创新。
+                </p>
+                <br />
+                <h3>
+                  开放
+                </h3>符合金融及要求的安全可靠、高可用、高性能的服务能力，符合金融及要求的安全可靠、高可用、高性能的服务能力。
+              </span>
+            ),
+          },
+          img: {
+            className: 'content7-img',
+            md: 10,
+            xs: 24,
+            children:
+              'https://zos.alipayobjects.com/rmsportal/xBrUaDROgtFBRRL.png',
+          },
+        },
+      },
+    ],
+  },
+};
+export const Footer00DataSource = {
+  wrapper: { className: 'home-page-wrapper footer0-wrapper' },
+  OverPack: { className: 'home-page footer0', playScale: 0.05 },
+  copyright: {
+    className: 'copyright',
+    children: (
+      <span>
+        ©2018 <a href="https://motion.ant.design">Ant Motion</a> All Rights
+        Reserved
+      </span>
+    ),
+  },
+};
---
+++ src/desktop/pages/Home/index.js
@@ -0,0 +1,88 @@
+/* eslint no-undef: 0 */
+/* eslint arrow-parens: 0 */
+import React from 'react';
+import { enquireScreen } from 'enquire-js';
+
+import Nav3 from './Nav3';
+import Banner5 from './Banner5';
+import Feature3 from './Feature3';
+import Feature5 from './Feature5';
+import Footer0 from './Footer0';
+
+import {
+  Nav30DataSource,
+  Banner50DataSource,
+  Feature30DataSource,
+  Feature50DataSource,
+  Footer00DataSource,
+} from './data.source';
+import './less/antMotionStyle.less';
+
+let isMobile;
+enquireScreen((b) => {
+  isMobile = b;
+});
+
+const { location } = window;
+
+export default class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isMobile
+    };
+  }
+
+  componentDidMount() {
+
+    enquireScreen((b) => {
+      this.setState({ isMobile: !!b });
+    });
+
+  }
+
+  render() {
+    const children = [
+      <Nav3
+        id="Nav3_0"
+        key="Nav3_0"
+        dataSource={Nav30DataSource}
+        isMobile={this.state.isMobile}
+      />,
+      <Banner5
+        id="Banner5_0"
+        key="Banner5_0"
+        dataSource={Banner50DataSource}
+        isMobile={this.state.isMobile}
+      />,
+      <Feature3
+        id="Feature3_0"
+        key="Feature3_0"
+        dataSource={Feature30DataSource}
+        isMobile={this.state.isMobile}
+      />,
+      <Feature5
+        id="Feature5_0"
+        key="Feature5_0"
+        dataSource={Feature50DataSource}
+        isMobile={this.state.isMobile}
+      />,
+      <Footer0
+        id="Footer0_0"
+        key="Footer0_0"
+        dataSource={Footer00DataSource}
+        isMobile={this.state.isMobile}
+      />,
+    ];
+    return (
+      <div
+        className="templates-wrapper"
+        ref={(d) => {
+          this.dom = d;
+        }}
+      >
+
+      </div>
+    );
+  }
+}
