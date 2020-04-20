d879dd7129a786631d5998cecbf29eadc71d6a10
username@email.com <570797480@qq.com>
2020-04-20 16:15:56 +0800

    update appointments

--- src/desktop/pages/users/My/Appointments/Appointments.js
+++ src/desktop/pages/users/My/Appointments/Appointments.js
@@ -9,80 +9,35 @@ import {
 } from "antd"
 
 
-const messages = {
-    'my.appointments.title.image': "Image",
-    'my.appointments.title.customerName': "Customer Name",
-    'my.appointments.title.petName': "Pet Name",
-    'my.appointments.title.petGender': "Pet Gender",
-    'my.appointments.title.species': "Species",
-    'my.appointments.title.address': "Address",
-    'my.appointments.title.description': "Description",
-    'my.appointments.title.surgeryTime': "SurgeryTime",
-    'my.appointments.title.appointmentDate': "AppointmentDate",
-    'my.appointments.title.details': "Details",
-    'my.appointments.title.doctorAdvice': "DoctorAdvice",
-    'my.appointments.title.drawers': "Drawers",
-    'my.appointments.title.orderNumber': "No.",
-};
+
 
 const { Column } = Table;
-const data = [
-    {
-      orderNumber: '1',
-      customerName: 'John',
-      petName: 'Kitty',
-      petGender: 'Male',
-      species: 'Cat',
-      address: 'Beijing',
-      description: 'Good pet',
-      image: '',
-      surgeryTime: '2020.4.8',
-      appointmentDate: '2020.1.31'
-    },
-    {
-      orderNumber: '2',
-      customerName: 'Zhang San',
-      petName: 'Wang Cai',
-      petGender: 'Male',
-      species: 'Dog',
-      address: 'Shanghai',
-      description: 'Pretty pet',
-      image: '',
-      surgeryTime: '2020.5.6',
-      appointmentDate: '2020.4.29'
-    },
-    {
-      orderNumber: '3',
-      customerName: 'John',
-      petName: 'Kitty',
-      petGender: 'Male',
-      species: 'Cat',
-      address: 'Beijing',
-      description: 'Good pet',
-      image: '',
-      surgeryTime: '2020.4.8',
-      appointmentDate: '2020.1.31'
-    },
-
-  ];
 
 
 export default (props) => {
 
     const [showDrawer, setShowDrawer] = useState(false);
+    const [record, setRecord] = useState(0);
+    const { data } = props;
+    const datad = [];
+    if (data.lenth !== 0) {
+        datad.push(...data);
+    } else {
+        datad.push({});
+    }
 
-
-    const onMoreClick = (records) =>{
+    const onMoreClick = (records) => {
         console.log(records);
+        setRecord(records);
         setShowDrawer(true);
     }
 
-    const onDrawerClose = () =>{
-        setShowDrawer(false)
+    const onDrawerClose = () => {
+        setShowDrawer(false);
     }
 
 
-    const languages = messages;
+    const languages = props.messages;
 
     const onFinish = values => {
         console.log(values);
@@ -93,47 +48,63 @@ export default (props) => {
             <Row>
                 <Col span={24} offset={0}>
                     <Table dataSource={data}
-                        pagination={{position:['bottomcenter']}}
+                        onChange={props.onPageChange}
+                        scroll={{ x: 1600 }}
+                        pagination={{ position: ['bottomcenter'] }}
                     >
-                        <Column ellipsis={true} title={languages["my.appointments.title.orderNumber"]} dataIndex="orderNumber" key="orderNumber" />
-                        <Column ellipsis={true} title="" dataIndex="image" key="image" />
-                        <Column ellipsis={true} title={languages["my.appointments.title.petName"]} dataIndex="petName" key="petName" />
-                        <Column ellipsis={true} title={languages["my.appointments.title.petGender"]} dataIndex="petGender" key="petGender" />
-                        <Column ellipsis={true} title={languages["my.appointments.title.species"]} dataIndex="species" key="species" />
-                        <Column ellipsis={true} title={languages["my.appointments.title.address"]} dataIndex="address" key="address" />
-                        <Column ellipsis={true} title={languages["my.appointments.title.appointmentDate"]} dataIndex="appointmentDate" key="appointmentDate" />
-                        <Column ellipsis={true} title={languages["my.appointments.title.surgeryTime"]} dataIndex="surgeryTime" key="surgeryTime" />
-                        <Column title="" render={record =>(
-                            <Button type="link" onClick={(e) =>(onMoreClick(record))}>{languages["my.appointments.title.details"]}</Button>
-                        )
-                        }
-                            />
+                        <Column title={languages["my.appointments.colTitle.id"]} dataIndex="id" key="id" fixed='left' width={100} />
+                        <Column title={languages["my.appointments.colTitle.petName"]} dataIndex="petName" key="petName" width={100} />
+                        <Column title={languages["my.appointments.colTitle.status"]} dataIndex="status" key="status" width={100} />
+                        <Column title={languages["my.appointments.colTitle.type"]} dataIndex="type" key="type" width={100} />
+                        <Column title={languages["my.appointments.colTitle.species"]} dataIndex="species" key="species" width={100} />
+                        <Column title={languages["my.appointments.colTitle.gender"]} dataIndex="gender" key="gender" width={100} />
+                        <Column title={languages["my.appointments.colTitle.meetingCity"]} dataIndex="meetingCity" key="meetingCity" />
+                        <Column title={languages["my.appointments.colTitle.appointmentTime"]} dataIndex="appointmentTime" key="id" />
+                        <Column title={languages["my.appointments.colTitle.createTime"]} dataIndex="createTime" key="createTime" />
+                        <Column title={languages["my.appointments.colTitle.needOperation"]} dataIndex="needOperation" key="needOperation" />
+                        <Column title={languages["my.appointments.colTitle.operationId"]} dataIndex="operationId" key="operationId" />
+                        <Column title=""
+                            render={(a, b, record) => (
+
+                                <Button type="link" onClick={(e) => (onMoreClick(record))}>{languages["my.appointments.row.more"]}</Button>
+
+                            )}
+                            fixed='right'
+                            width={100}
+
+                        />
                     </Table>
                 </Col>
             </Row>
-            
-                    <Drawer
-                        width="50%"
-                        title={languages["my.appointments.title.drawers"]}
-                        placement='left'
-                        closable={false}
-                        onClose={onDrawerClose}
-                        visible={showDrawer}
-                        >
-                        
-                        <Descriptions bordered={true} layout="horizontal" column={2}>
-                            <Descriptions.Item label={languages["my.appointments.title.customerName"]} span={2}>45631231231231</Descriptions.Item>
-                            <Descriptions.Item label={languages["my.appointments.title.petName"]} spam={2}>123321312312312</Descriptions.Item>
-                            <Descriptions.Item label={languages["my.appointments.title.petGender"]} span={2}>73231231289</Descriptions.Item>
-                            <Descriptions.Item label={languages["my.appointments.title.species"]} span={2}>453213213126</Descriptions.Item>
-                            <Descriptions.Item label={languages["my.appointments.title.address"]} span={2}>132131231223</Descriptions.Item>
-                            <Descriptions.Item label={languages["my.appointments.title.appointmentDate"]}>732131232189</Descriptions.Item>
-                            <Descriptions.Item label={languages["my.appointments.title.surgeryTime"]}>4321312356</Descriptions.Item>
-                            <Descriptions.Item label={languages["my.appointments.title.description"]} span={2}>123213123</Descriptions.Item>
-                        </Descriptions>
-                    </Drawer>
-                        
-                
+
+            <Drawer
+                width="50%"
+                title={languages["my.appointments.title.drawers"]}
+                placement='left'
+                closable={false}
+                onClose={onDrawerClose}
+                visible={showDrawer}
+            >
+
+                <Descriptions bordered={true} layout="horizontal" column={2}>
+                    <Descriptions.Item label={languages["my.appointments.colTitle.id"]} span={2}>{datad[record].id || ""}</Descriptions.Item>
+                    <Descriptions.Item label={languages["my.appointments.colTitle.contactNumber"]} span={2}>{datad[record].contactNumber || ""}</Descriptions.Item>
+                    <Descriptions.Item label={languages["my.appointments.colTitle.type"]}>{datad[record].type || ""}</Descriptions.Item>
+                    <Descriptions.Item label={languages["my.appointments.colTitle.status"]} >{datad[record].status || ""}</Descriptions.Item>
+                    <Descriptions.Item label={languages["my.appointments.colTitle.petName"]}>{datad[record].petName || ""}</Descriptions.Item>
+                    <Descriptions.Item label={languages["my.appointments.colTitle.species"]} span={2}>{datad[record].species || ""}</Descriptions.Item>
+                    <Descriptions.Item label={languages["my.appointments.colTitle.gender"]} >{datad[record].gender || ""}</Descriptions.Item>
+                    <Descriptions.Item label={languages["my.appointments.colTitle.meetingCity"]} span={2}>{datad[record].meetingCity || ""}</Descriptions.Item>
+                    <Descriptions.Item label={languages["my.appointments.colTitle.needOperation"]} >{datad[record].needOperation || ""}</Descriptions.Item>
+                    <Descriptions.Item label={languages["my.appointments.colTitle.operationId"]} >{datad[record].operationId || ""}</Descriptions.Item>
+                    <Descriptions.Item label={languages["my.appointments.colTitle.appointmentTime"]} span={2}>{datad[record].appointmentTime || ""}</Descriptions.Item>
+                    <Descriptions.Item label={languages["my.appointments.colTitle.createTime"]} span={2}>{datad[record].createTime || ""}</Descriptions.Item>
+                    <Descriptions.Item label={languages["my.appointments.colTitle.description"]} span={2}>{datad[record].description || ""}</Descriptions.Item>
+                    <Descriptions.Item label={languages["my.appointments.colTitle.diagnosis"]} span={2}>{datad[record].diagnosis || ""}</Descriptions.Item>
+                </Descriptions>
+            </Drawer>
+
+
         </div>
-        );
-    };
+    );
+};
