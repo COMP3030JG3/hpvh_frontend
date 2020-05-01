2a7747185294bd7bedf07df223b3ec72af652a6e
Xingchen.Lu <wsteth@outlook.com>
2020-05-01 16:46:51 +0800

    add discussion

---
+++ src/desktop/pages/Discussion/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import DiscussionContainer from "./DiscussionContainer"
+
+export default (props) => {
+    return (
+        <DiscussionContainer {...props} />
+    );
+};
